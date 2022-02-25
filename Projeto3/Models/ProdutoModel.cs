a4100f4acf461f924d45ef83051e06ffe86e3fdd
0220482113005 <0220482113005@LAB4TC22>
2022-02-25 20:07:53 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto3/Models/ProdutoModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projeto3.Models
+{
+    public class ProdutoModel
+    {
+        public int codigo { get; set; }
+        public decimal valor { get; set; }
+        public string nome { get; set; }
+    }
+}
